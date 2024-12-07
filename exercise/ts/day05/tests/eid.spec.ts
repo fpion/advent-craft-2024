d6fbe48fd5140bbca1c9b2aed5a37924c9ddaae0
f.pion <florent.pion@gmail.com>
2024-12-07 16:27:45 +0100

    Test of Year is ok

--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -18,9 +18,13 @@ function validateElfSex(EID: string) {
     return Number(EID.slice(0, 1)) in Sex;
 }
 
+function validateElfYear(EID: string) {
+    return Number(EID.slice(1, 3)) > 0 && Number(EID.slice(1, 3)) < 100;
+}
+
 //TODO :4, 5, 6	"Serial number": birth order	From 001 to 999
 const validateEID = (EID: string) => {
-    return validateEIDLength(EID) && validateElfSex(EID); 
+    return validateEIDLength(EID) && validateElfSex(EID) && validateElfYear(EID);
 }
 
 //TODO  :7, 8	control key = complement to 97 of the number formed by the first 6 digits of the EID modulo 97	From 01 to 97
@@ -35,5 +39,11 @@ describe('EID', () => {
         expect(validateEID('22345678')).toBe(true);
         expect(validateEID('32345678')).toBe(true);
         expect(validateEID('42345678')).toBe(false);
-    }); 
+    });
+
+    test('Fist Digit of an EID is 1,2,3 for Sex of Elf', () => {
+        expect(validateEID('12445678')).toBe(true);
+        expect(validateEID('22x45678')).toBe(false);
+        expect(validateEID('3b345678')).toBe(false);
+    });
 });
